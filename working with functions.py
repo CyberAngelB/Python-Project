f2b1613e3356c5a9abfba88644973c5cc5e17f45
Ali Victor Bamidele <admiraldele@gmail.com>
2023-03-09 11:27:10 +0100

    functions basic for beginner

---
+++ working with functions.py
@@ -0,0 +1,13 @@
+#Working with python functions
+def personal_info(name, age, des):
+    print("Hello Everyone i'm " + name + "I'm just" + age + " A student of " + des )
+
+personal_info("bammy ", " 21 ", "Eksu")
+personal_info("Victor ", " 20 ", "KSU" )
+
+def about_school(course, level, babe):
+    print("Im Currently studying " + course + "Also in " + level + "I love calling my babe " + babe)
+about_school("Computer Science ", "200L ", "SuperGirl")
+# simple beginner program working with functions
+
+
